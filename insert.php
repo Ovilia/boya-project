2f57fb1d398b241f8edf5a0929055b4d2f2244cb
zwl.sjtu@gmail.com <zwl.sjtu@gmail.com@a5f845e8-0e4a-9cb6-2f2b-ebf29cc3aad3>
2011-07-07 11:31:28 +0000

    Important change of cdm ldm pdm, test data involved.

--- insert.php
+++ insert.php
@@ -1,27 +1,26 @@
 <?php
 require_once('function.php');
 
-function testUser(){
-	for ($i = 0; $i < 5; ++$i){
-		if (!insertUser('user'.$i, hash('sha512', $i), 
-			'user'.$i.'@boya.com', null, null, null))
-			$i--;
+function startInsert($offset, $amt){	
+	for ($i = 0; $i < $amt; ++$i){
+		$index = $offset + $i;
+		
+		insertUser('username'.$index, 'password'.$index, 'email'.$index.'@boya.com');					
+		insertQuestion('content of question'.$index.'一些中文问题的内容');			
+	}
+	
+	for ($i = 0; $i < $amt; ++$i){	
+		for ($j = 0; $j < rand(0, $amt); ++$j){	
+			if (rand(0, 1))
+				$ans = 'Y';
+			else
+				$ans = 'N';
+			insertAnswer($i + 1, $j + 1, $ans);
+			
+			setFollow($i + 1, $j + 1);
+		}
 	}
 }
 
-function testQues(){
-	for ($i = 0; $i < 5; ++$i){
-		insertQuestion($i, 'null', hash('sha512', $i));
-	}
-}
-
-function testAns(){
-	for ($i = 0; $i < 10; ++$i){
-		echo insertAnswer($i, $i, 0);
-		echo insertAnswer($i, 2*$i+1, 1);
-	}
-}
-testUser();
-testQues();
-
+startInsert(0, 100);
 ?>
